c16cee23b44c8c9ac892b21b1e7311193760e836
scheals <piecoo@gmail.com>
2022-03-06 17:05:37 +0100

    Introduce building a Tree

--- lib/tree.rb
+++ lib/tree.rb
@@ -4,8 +4,25 @@ require_relative './node'
 
 # This class handles balanced binary trees.
 class Tree
+  attr_reader :root
 
   def initialize(node = nil)
     @root = node
+  end
+
+  def build_tree(array, front, back)
+    return nil if front > back
+
+    middle = (front + back) / 2
+    node = Node.new(array[middle])
+    node.left_node = build_tree(array, front, middle - 1)
+    node.right_node = build_tree(array, middle + 1, back)
+    @root = node
+  end
+
+  def pretty_print(node = @root, prefix = '', is_left = true)
+    pretty_print(node.right_node, "#{prefix}#{is_left ? '│   ' : '    '}", false) if node.left_node
+    puts "#{prefix}#{is_left ? '└── ' : '┌── '}#{node.value}"
+    pretty_print(node.left_node, "#{prefix}#{is_left ? '    ' : '│   '}", true) if node.left_node
   end
 end
--- main.rb
+++ main.rb
@@ -3,5 +3,9 @@
 require_relative 'lib/node'
 require_relative 'lib/tree'
 
-a = Node.new(1)
-b = Node.new(2)
+test_array = [1, 7, 4, 23, 8, 9, 4, 3, 5, 7, 9, 67, 6345, 324].uniq.sort
+back = test_array.length - 1
+tree = Tree.new
+p tree.build_tree(test_array, 0, back)
+tree.pretty_print
+p tree
