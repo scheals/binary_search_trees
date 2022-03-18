c4e16992c0c815f4d2ad6cba690bd0d39fb90ad1
scheals <piecoo@gmail.com>
2022-03-18 23:02:19 +0100

    Introduce two testing scripts

--- main.rb
+++ main.rb
@@ -4,16 +4,15 @@ require_relative 'lib/node'
 require_relative 'lib/tree'
 
 def test
-  array = (Array.new(15) { rand(1..100) }).uniq.sort
-  back = array.length - 1
+  array = (Array.new(15) { rand(1..100) })
   tree = Tree.new
-  tree.build_tree(array, 0, back)
+  tree.build_tree(array)
   tree.pretty_print
   p tree.balanced?
-  p tree.level_order
-  p tree.preorder
-  p tree.inorder
-  p tree.postorder
+  p tree.level_order { |node| node.value * 2 }
+  p tree.preorder { |node| node.value * 2 }
+  p tree.inorder { |node| node.value * 2 }
+  p tree.postorder { |node| node.value * 2 }
   10.times do
     tree.insert(rand(101..200))
   end
@@ -29,3 +28,33 @@ def test
   puts 'Test finished!'
 end
 test
+def delete_test
+  array = [1, 11, 8, 23, 34, 56, 88, 10, 4, 18, 55, 56]
+  tree = Tree.new
+  tree.build_tree(array)
+  tree.pretty_print
+  p tree.balanced?
+  p tree.delete(11)
+  tree.pretty_print
+  p tree.balanced?
+  p tree.delete(56)
+  tree.pretty_print
+  p tree.balanced?
+  p tree.delete(10)
+  tree.pretty_print
+  p tree.balanced?
+  p tree.delete(18)
+  tree.pretty_print
+  p tree.delete(55)
+  tree.pretty_print
+  p tree.height(tree.find(4))
+  p tree.depth(tree.find(4))
+  p tree.height(tree.find(23))
+  p tree.depth(tree.find(23))
+  p tree.height(tree.find(88))
+  p tree.depth(tree.find(88))
+  p tree.balanced?
+  tree.rebalance
+  tree.pretty_print
+end
+delete_test
